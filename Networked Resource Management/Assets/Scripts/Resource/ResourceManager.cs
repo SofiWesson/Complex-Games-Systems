cbb4d11341c5b5fc852d6aca6bdd6b2faf6bbcf0
Sofi Wesson <sofiwesson@gmail.com>
2022-05-24 12:50:36 +1000

    attributes are removable from all

--- Networked Resource Management/Assets/Scripts/Resource/ResourceManager.cs
+++ Networked Resource Management/Assets/Scripts/Resource/ResourceManager.cs
@@ -10,72 +10,91 @@ public class ResourceManager : ScriptableObject
     public List<Attribute.AttributeObj> attributes = new List<Attribute.AttributeObj>();
     [SerializeField]
     public List<Resource.ResourceObj> resources = new List<Resource.ResourceObj>();
-
-    protected Test resourceLock;
-    public struct Test
-    {
-        public List<Resource.ResourceObj> test;
-    }
     
     [Space(10)]
     public Attribute.AttributeObj myCustomAttribute;
     public Resource.ResourceObj myCustomResource;
 
+    [Serializable]
+    public struct AttributeRemovable
+    {
+        [NonReorderable] public string name;
+        public bool removeThis;
+    }
+
     [Space(10)]
-    public Attribute.AttributeObj attributeToRemove;
-  
+    [NonReorderable]
+    public List<AttributeRemovable> removeAttribute;
+
     public void AddAttributes()
     {
         attributes.Add(myCustomAttribute);
         myCustomAttribute = new Attribute.AttributeObj();
-
-        List<Attribute.AttributeObj> tempList = new List<Attribute.AttributeObj>();
-        foreach (Attribute.AttributeObj attribute in attributes)
-        {
-            Attribute.AttributeObj tempAtt = new Attribute.AttributeObj();
-            tempAtt.name = attribute.name;
-            tempAtt.variable = attribute.variable;
-            tempList.Add(tempAtt);
-        }
-        myCustomResource.attributes = tempList;
+        UpdateResourceAttributesList();
+        // needs to add to resources
     }
 
     public void AddResource()
     {
         resources.Add(myCustomResource);
         myCustomResource = new Resource.ResourceObj();
+        UpdateResourceAttributesList();
+    }
 
-        List<Attribute.AttributeObj> tempList = new List<Attribute.AttributeObj>();
+    public void RemoveAttribute()
+    {
+        foreach (AttributeRemovable removableAttribute in removeAttribute)
+        {
+            for (int i = attributes.Count - 1; i >= 0; i--) //Attribute.AttributeObj attribute in attributes)
+            {
+                if (attributes[i].name == removableAttribute.name && removableAttribute.removeThis)
+                {
+                    attributes.Remove(attributes[i]);
+                    continue;
+                }
+            }
+
+            foreach (Resource.ResourceObj resource in resources)
+            {
+                for (int i = resource.attributes.Count - 1; i >= 0; i--)
+                {
+                    if (resource.attributes[i].name == removableAttribute.name && removableAttribute.removeThis)
+                    {
+                        resource.attributes.Remove(resource.attributes[i]);
+                        continue;
+                    }
+                }
+            }
+        }
+
+        UpdateResourceAttributesList();
+    }
+
+    void UpdateResourceAttributesList()
+    {
+        List<Attribute.AttributeObj> tempAttList = new List<Attribute.AttributeObj>();
+        List<AttributeRemovable> tempRemList = new List<AttributeRemovable>();
         foreach (Attribute.AttributeObj attribute in attributes)
         {
             Attribute.AttributeObj tempAtt = new Attribute.AttributeObj();
             tempAtt.name = attribute.name;
             tempAtt.variable = attribute.variable;
-            tempList.Add(tempAtt);
+            tempAttList.Add(tempAtt);
+
+            AttributeRemovable tempRem = new AttributeRemovable();
+            tempRem.name = attribute.name;
+            tempRem.removeThis = false;
+            tempRemList.Add(tempRem);
         }
-        myCustomResource.attributes = tempList;
-    }
-
-    public void RemoveAttribute()
-    {
-        attributes.Remove(attributeToRemove);
-        attributeToRemove = new Attribute.AttributeObj();
-    }
-
-    void Start()
-    {
-        
-    }
-
-    [ExecuteInEditMode]
-    void Update()
-    {
-        
+        myCustomResource.attributes = tempAttList;
+        removeAttribute = tempRemList;
     }
 
     public void ClearAll()
     {
         attributes.Clear();
         resources.Clear();
+        myCustomResource.attributes.Clear();
+        removeAttribute.Clear();
     }
 }
